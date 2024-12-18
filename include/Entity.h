0611594b1ad5fc50b5cdf3306b5c1343e0592c7b
ilya <ivchvanov@edu.hse.ru>
2024-12-18 22:11:01 +0300

    implement(Entity)

--- include/Entity.h
+++ include/Entity.h
@@ -1,24 +1,33 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include <Box2D/Box2D.h>
+#include <box2d/box2d.h>
+
+#include <filesystem>
 #include <memory>
+#include <unordered_map>
+#include <bits/fs_path.h>
+
+using std::shared_ptr;
+using path = std::filesystem::path;
 
 class Entity {
 public:
-    Entity() = default;
+    explicit Entity(const std::unordered_map<std::string, path>& texturePaths);
     virtual ~Entity() = default;
 
     virtual void update(float deltaTime) = 0;
     virtual void render(sf::RenderWindow& window) = 0;
+    virtual void move(float deltaX, float deltaY) = 0;
+    virtual void shoot() = 0;
+    virtual void onCollision(Entity& other) = 0;
 
     void setPosition(const sf::Vector2f& pos);
-    sf::Vector2f getPosition() const;
-
     void createBody(b2World& world, const b2BodyDef& bodyDef);
-    b2Body* getBody() { return m_body; }
+    shared_ptr<b2Body> getBody();
 
 protected:
-    sf::Sprite m_sprite;
-    b2Body* m_body{ nullptr };
+    sf::Sprite mSprite;
+    shared_ptr<b2Body> mBody;
+    std::unordered_map<std::string, sf::Texture> m_textures; // map текстур
 };
---
+++ src/Entity.cpp
@@ -0,0 +1,30 @@
+#include "Entity.h"
+
+Entity::Entity(const std::unordered_map<std::string, path>& texturePaths) {
+    for (const auto&[name, path] : texturePaths) {
+        sf::Texture texture;
+        if (!texture.loadFromFile(path.string())) {
+            throw std::runtime_error("Failed to load texture from " + path.string());
+        }
+        m_textures[name] = texture;
+    }
+    if (!m_textures.empty()) {
+        mSprite.setTexture(m_textures.begin()->second);
+    }
+}
+
+void Entity::setPosition(const sf::Vector2f& pos) {
+    mSprite.setPosition(pos);  // Устанавливаем позицию спрайта
+    if (mBody) {
+        mBody->SetTransform(b2Vec2(pos.x, pos.y), mBody->GetAngle());  // Устанавливаем позицию тела Box2D (если оно существует)
+    }
+}
+
+void Entity::createBody(b2World& world, const b2BodyDef& bodyDef) {
+    mBody = shared_ptr<b2Body>(world.CreateBody(&bodyDef), [&world](b2Body* body) {
+        world.DestroyBody(body);  // Удаляем тело при разрушении сущности
+    });
+}
+shared_ptr<b2Body> Entity::getBody() {
+    return mBody;
+}
